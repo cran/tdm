b68a20ac82f56244306135d7fbc7cabbf3b876ff
Yung-jin Lee <mobilepk@gmail.com>
2011-12-22 00:00:00 +0000

    version 2.2.3

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,19 @@
 .First.lib <- function(...) {
 
-   # echo output to screen
-   cat("\n")
-   cat("*************************** tdm ***********************************\n")
-   cat("           An R Tool for Therapeutic Drug Monitoring               \n\n")
-   cat("                          v 2.2.2                                  \n\n")   
-   cat("  TDM is created for your own personal uses and testing purposes.  \n")
-   cat("  TDM shall be used as a guide or a decision support tool only.    \n")  
-   cat("  Medical decisions should NOT be solely based on the results of   \n")
-   cat("  this program. Although this program has been tested thoroughly,  \n")
-   cat("  the accuracy of the information cannot be guaranteed. Once you   \n")
-   cat("  use tdm, you have automatically agreed with this disclaimer.     \n\n")   
-   cat("             Please type 'run()' to get started.                   \n")
-   cat("*******************************************************************\n\n")
+# echo output to screen
+packageStartupMessage("
+
+*************************** tdm ***********************************
+           An R Tool for Therapeutic Drug Monitoring               
+                          v 2.2.3                                   
+  TDM is created for your own personal uses and testing purposes.  
+  TDM shall be used as a guide or a decision support tool only.    
+  Medical decisions should NOT be solely based on the results of   
+  this program. Although this program has been tested thoroughly,  
+  the accuracy of the information cannot be guaranteed. Once you   
+  use tdm, you have automatically agreed with this disclaimer.      
+             Please type 'run()' to get started.                   
+*******************************************************************")
 
 }
 
